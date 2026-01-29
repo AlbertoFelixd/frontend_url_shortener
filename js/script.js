02974cef1e8592b0c2e10d753434806569ec897a
AlbertoFelixd <alberto.felixd@hotmail.com>
2026-01-28 22:26:48 -0600

    Primer commit frontend url_shortener

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+const API_URL = 'http://localhost:8080/url_shortener/';
+
+// Formulario url
+document.getElementById('urlForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // No recargar la pagina
+
+    const longUrl = document.getElementById('longUrl').value;
+
+    if(!longUrl) {
+        console.log('URL invalido');
+        return;
+    }
+
+    // if(longUrl) {
+    //     console.log('URL a acortar:', longUrl);
+    //     console.log('Listo');
+    //     return;
+    // }
+
+    try {
+        const response = await fetch(API_URL + 'shorten', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ longUrl: longUrl })
+        });
+        
+        const data = await response.json();
+
+        const fullShortUrl = API_URL + data.code;
+
+        alert('URL acortada: ' + fullShortUrl);
+
+        console.log('URL original:', data.longUrl);
+        console.log('Código:', data.code);
+        console.log('URL corta:', fullShortUrl);
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}); 
